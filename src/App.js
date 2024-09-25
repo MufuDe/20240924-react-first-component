fa17eb143a1c3d146fcebf9409b9ace334ed5ede
chendechuan <chen0802@qq.com>
2024-09-25 09:36:53 +0800

    1. 在JSX中通过大括号使用javascript；

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Gallery from "./component/Gallery";
 import { Profile } from "./component/Profile";
 import TodoList from "./component/TodoList";
+import Avatar from "./component/Avatar";
 
 export default function App() {
   return (
@@ -16,6 +17,10 @@ export default function App() {
       <section>
         <h1>TodoList</h1>
         <TodoList />
+      </section>
+      <section>
+        <h1>Avatar</h1>
+        <Avatar />
       </section>
     </>
   );
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,9 +1,23 @@
 export default function TodoList() {
+  const name = "海蒂·拉玛";
+  const today = new Date();
+
+  function formatDate(date) {
+    return new Intl.DateTimeFormat("zh-CN", { weekday: "long" }).format(date);
+  }
+
   return (
     <>
-      <h1>海蒂·拉玛的待办事项</h1>
+      <h1>
+        {name}的{formatDate(today)}待办事项
+      </h1>
       <img src="/img/yXOvdOSs.jpg" alt="Hedy Lamarr" className="photo" />
-      <ul>
+      <ul
+        style={{
+          backgroundColor: "black",
+          color: "pink",
+        }}
+      >
         <li>发明一种新式交通信号灯</li>
         <li>排练一个电影场景</li>
         <li>改进频谱技术</li>

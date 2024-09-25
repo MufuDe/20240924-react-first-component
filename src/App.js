2d7a1a50a1ed8e0de1844cd2c716b76bf07a03af
chendechuan <chen0802@qq.com>
2024-09-25 15:07:41 +0800

    1. 条件渲染；

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import { useState, useEffect } from "react";
-import Clock from "./component/Clock";
-
-function useTime() {
-  const [time, setTime] = useState(() => new Date());
-  useEffect(() => {
-    const id = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
-  return time;
-}
+import PackingList from "./component/PackingList";
 
 export default function App() {
-  const time = useTime();
-  const [color, setColor] = useState("lightcoral");
   return (
-    <div>
-      <p>
-        选择一个颜色:{" "}
-        <select value={color} onChange={(e) => setColor(e.target.value)}>
-          <option value="lightcoral">浅珊瑚色</option>
-          <option value="midnightblue">午夜蓝</option>
-          <option value="rebeccapurple">丽贝卡紫</option>
-        </select>
-      </p>
-      <Clock color={color} time={time.toLocaleTimeString()} />
-    </div>
+    <>
+      <PackingList />
+    </>
   );
 }
